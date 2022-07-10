2f24db81e5a64c6ab42bc5b764ff71fe4fc53dee
Iyadh Hakiri <iyadhbox@gmail.com>
2022-07-10 15:52:42 +0000

    added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#233e6e",
+        secondary: "#3eabde",
+        light: "#f8ffff",
+      },
+    },
+  },
+  plugins: [],
+};
